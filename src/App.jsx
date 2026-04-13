3762fe9677a6d4763e0d088346680861257a8db6
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-04-13 14:57:31 +0530

    feat: add first name input with live preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-
+import { useState } from 'react'
 
 const App = () => {
+  const [firstName, setFirstName] = useState("");
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value)
+  }
+
   return (
     <div>
-      <p>Cv Builder App</p>
+      <h1>Cv Builder App</h1>
+      <div>
+        <h3>Personal Information</h3>
+        <label>First Name</label>
+        <input type="text"
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+      </div>
+      <div>
+        <hr />
+        <h3>Preview</h3>
+        <p>{firstName}</p>
+      </div>
     </div>
   )
 }
