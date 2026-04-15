a6ca1fa973d59883c7db691fdc8f0dd707dce057
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-04-15 16:13:46 +0530

    feat: add Input component for personal information

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Input = ({label,value,onChange}) => {
+console.log("Input value :",{value});
+  return (
+    <>
+     <label>{label}</label>
+     <input type="text"
+          value={value}
+          onChange={onChange}
+        />
+    </>
+    
+  )
+}
+
 const App = () => {
   const [firstName, setFirstName] = useState("");
 
@@ -12,11 +26,16 @@ const App = () => {
       <h1>Cv Builder App</h1>
       <div>
         <h3>Personal Information</h3>
-        <label>First Name</label>
-        <input type="text"
-          value={firstName}
-          onChange={handleFirstNameChange}
-        />
+        <Input label="First Name" value={firstName}  onChange={handleFirstNameChange}/>
+        <label>Last Name</label>
+        <input type="text" />
+        <label>Contact Number</label>
+        <input type="tel" />
+        <label>Github Link</label>
+        <input type="text" />
+        <label>LinkedIn Link</label>
+        <input type="text" />
+        
       </div>
       <div>
         <hr />
